ccda66151dd62317c6afcb5e8633e89383fd2b5e
Babatunde <babatundea15@gmail.com>
2020-07-07 15:40:18 +0100

    error handling if there is no positive api response

--- weatherInfo.js
+++ weatherInfo.js
@@ -1,1 +1,18 @@
-import axios from "axios";
+const axios = require("axios");
+var result;
+
+function getWeather(cityName) {
+  return axios
+    .get(
+      "http://api.weatherstack.com/current?access_key=d8bc6469d4e4525363fce1dbe9ad3b67&query=" +
+        cityName
+    )
+    .then((response) => {
+      if (response.data.success === false) {
+        console.log("Please provide a valid location");
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
